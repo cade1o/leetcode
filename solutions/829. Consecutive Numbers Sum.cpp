a1b1e944c86160b1988267b5cc6400b6545187a5
cade1o <50832103+cade1o@users.noreply.github.com>
2020-12-17 12:51:39 +1030

    8 ms

--- solutions/829. Consecutive Numbers Sum.cpp
+++ solutions/829. Consecutive Numbers Sum.cpp
@@ -8,32 +8,32 @@ public:
        
         if ( fmod(K,2.0) != 0.0 )
         {      
-                for ( double i = 1 ; i < sqrt(K) + 1 ; i += 2 )
+            for ( double i = 1 ; i < sqrt(K) + 1 ; i += 2 )
+            {
+                if ( fmod(K,i) == 0.0  ) 
                 {
-                    if ( fmod(K,i) == 0.0  ) 
-                    {
-                        ways ++ ;
-                        if ( fmod(K/i,2.0) != 0.0 & i != 1 && K/i != i) ways ++ ;
-                    }
+                    ways ++ ;
+                    if ( fmod(K/i,2.0) != 0.0 & i != 1 && K/i != i) ways ++ ;
                 }
-                ways ++ ;
+            }
+            ways ++ ;
         }
         else
         {
             while( fmod(K,2.0) == 0.0 ) K = K / 2 ;
            
-                if( K > 1 )
+            if( K > 1 )
+            {
+                for ( double i = 1 ; i < sqrt(K) + 1 ; i += 2 )
                 {
-                    for ( double i = 1 ; i < sqrt(K) + 1 ; i += 2 )
+                    if ( fmod(K,i) == 0.0  ) 
                     {
-                        if ( fmod(K,i) == 0.0  ) 
-                        {
-                            ways ++ ;
-                            if ( fmod(K/i,2.0) != 0.0 && i != 1 && K/i != i) ways ++ ;        
-                        }
-                    }  
-                }
-                ways ++ ;
+                        ways ++ ;
+                        if ( fmod(K/i,2.0) != 0.0 && i != 1 && K/i != i) ways ++ ;        
+                    }
+                }  
+            }
+            ways ++ ;
         }
         return ways ;
     }
