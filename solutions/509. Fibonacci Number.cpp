95823bc002a60f0dd38361448e024f4f8fcf9a4c
cade1o <50832103+cade1o@users.noreply.github.com>
2021-01-05 12:02:58 +1030

    0 ms

---
+++ solutions/509. Fibonacci Number.cpp
@@ -0,0 +1,17 @@
+//Author: Cade
+class Solution {
+public:
+    int fib(int n) {
+        int a = 0 , b = 1 , c = 0 ;
+        
+        if ( n == 0 ) return 0 ;
+        if ( n == 1 ) return 1 ;
+        
+        for ( int i = 2 ; i < n+1 ; i++){
+            c = a + b ;
+            a = b ;
+            b = c ;
+        }
+        return c ;
+    }
+};
