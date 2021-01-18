e72285ff44ee416bb7eb08eac80b43bb90c0eaeb
cade1o <50832103+cade1o@users.noreply.github.com>
2021-01-19 09:53:51 +1030

    28 ms

--- solutions/581. Shortest Unsorted Continuous Subarray.cpp
+++ solutions/581. Shortest Unsorted Continuous Subarray.cpp
@@ -2,28 +2,17 @@ class Solution {
 public:
     int findUnsortedSubarray(vector<int>& nums) {
         if( nums.size() == 1 ) return 0;
-        int mina = INT_MAX, end = INT_MIN;
-        stack<int> abc;
+        int st = 0, end = 0 ,ma = nums[0], mi = nums[nums.size()-1];
 ​
         for( int i = 0; i < nums.size(); ++i){
-            while( !abc.empty() && nums[i] < nums[abc.top()] ){
-                mina = min(mina,abc.top());
-                abc.pop();
-                end = i;
-            }
-            abc.push(i);
+            ma = max(ma,nums[i]);
+            mi = min(mi,nums[nums.size()-1-i]);
+            if( ma > nums[i] ) end = i;
+            if( mi < nums[nums.size()-1-i] ) st = nums.size()-1-i;
         }
-        while(!abc.empty()) abc.pop();
-        
-        for( int i = nums.size()-1; i > -1; --i){
-            while( !abc.empty() && nums[i] > nums[abc.top()] ){
-                end = max(end,abc.top());
-                abc.pop();
-            }
-            abc.push(i);
-        }
-        if( mina == INT_MAX ) return 0;
     
-        return end-mina+1;
+        if( end == st ) return 0;
+    
+        return end-st+1;
     }
 };
