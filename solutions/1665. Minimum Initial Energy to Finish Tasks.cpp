43de3e2583fd93523e40f3f16c5a8e897b5b9582
cade1o <50832103+cade1o@users.noreply.github.com>
2021-02-07 21:14:35 +1030

    356 ms

--- solutions/1665. Minimum Initial Energy to Finish Tasks.cpp
+++ solutions/1665. Minimum Initial Energy to Finish Tasks.cpp
@@ -2,16 +2,13 @@
 class Solution {
 public:
     
-    static bool com(vector<int>& a, vector<int>&b){
-        return a[1]-a[0] > b[1]-b[0];
-    }
-    
+​
     int minimumEffort(vector<vector<int>>& tasks) {
         
         int sum = 0;
 ​
-        int size = tasks.size();
-        sort(tasks.begin(), tasks.end(), com);
+        sort(begin(tasks), end(tasks), [](vector<int> &t1, vector<int> &t2) 
+        { return t1[1] - t1[0] > t2[1] - t2[0]; });
         
         for( auto &a: tasks){
             sum += a[0];
